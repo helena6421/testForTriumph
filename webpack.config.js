28203d2412ffcf671f31ad8dabfc569b11e78c72
helena6421 <40118858+helena6421@users.noreply.github.com>
2025-08-04 19:53:38 +0300

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
-  entry: './src/index.js',
+  mode: mode,
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/' 
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    })
-  ]
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devtool: mode === "development" ? "inline-source-map" : false,
 };
