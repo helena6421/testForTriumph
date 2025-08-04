23ec891598d8a88be6d847fb50ab11734b0982ae
Алёна Ласкина <40118858+helena6421@users.noreply.github.com>
2025-08-04 18:29:26 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,17 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: '/'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/' 
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
       filename: 'index.html'
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 9000,
-    client: {
-      overlay: false,
-    },
-    open: true,
-  },
-  target: "web",
-  mode: "development",
+    })
+  ]
 };
